b430ec6154ab012cbfe254ed075026204398f67e
SAStevens <118325820+SAStevens@users.noreply.github.com>
2023-01-28 17:23:09 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <h1>Hello from React</h1>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
